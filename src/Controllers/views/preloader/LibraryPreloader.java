956a7853c95e2f957483fc1998941716f8cd0f67
Kostya Nirchenko <nirchenko.kostya@gmail.com>
2015-08-10 16:55:31 +0300

    Добавление предзагрузчика

--- src/Controllers/views/preloader/LibraryPreloader.java
+++ src/Controllers/views/preloader/LibraryPreloader.java
@@ -1,8 +1,13 @@
 package Controllers.views.preloader;
 
+import Models.Database;
 import javafx.application.Preloader;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -14,17 +19,22 @@ import javafx.stage.Stage;
  */
 public class LibraryPreloader extends Preloader {
 
-    ProgressBar bar;
+    public PasswordField passField;
+    public TextField loginField;
+    public TextField urlField;
+    public Button connectButton;
+    public Button exitButton;
+    public ProgressBar bar;
     Stage stage;
 
-    private Scene createPreloader() {
+    /*private Scene createPreloader() {
 
-    }
+    }*/
 
     @Override
     public void start(Stage stage) throws Exception {
         this.stage = stage;
-        stage.setScene(createPreloader());
+//        stage.setScene(createPreloader());
         stage.show();
     }
 
@@ -36,5 +46,32 @@ public class LibraryPreloader extends Preloader {
         if(stateChangeNotification.getType() == StateChangeNotification.Type.BEFORE_START) {
             stage.hide();
         }
+    }
+
+    public void handleConnect(ActionEvent actionEvent) {
+        if(!urlField.getText().isEmpty() && !urlField.getText().equals("")) {
+            String url = urlField.getText();
+            Database.setUrl(url);
+            urlField.setStyle("-fx-control-border-color: green");
+            if(!loginField.getText().isEmpty() && !loginField.getText().equals("")) {
+                String login = loginField.getText();
+                Database.setLogin(login);
+                loginField.setStyle("-fx-control-border-color: green");
+                if(!passField.getText().isEmpty() && !passField.getText().equals("")) {
+                    String pass = passField.getText();
+                    Database.setPass(pass);
+                    passField.setStyle("-fx-control-border-color: green");
+                } else {
+                    passField.setStyle("-fx-control-border-color: red");
+                }
+            } else {
+                loginField.setStyle("-fx-control-border-color: red");
+            }
+        } else {
+            urlField.setStyle("-fx-control-border-color: red");
+        }
+    }
+
+    public void handleExit(ActionEvent actionEvent) {
     }
 }
